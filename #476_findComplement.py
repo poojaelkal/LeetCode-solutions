ba74591e04a7f338b5dafdeadc6ecca9ff470492
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-05-18 18:37:25 -0400

    committing #476 solution

---
+++ #476_findComplement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        
+        i = 1
+        while i <=num:
+            i = i << 1
+            print(i)
+        return (i-1)^num
