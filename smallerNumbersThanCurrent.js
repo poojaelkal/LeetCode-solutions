7c817efa377fb5c7171fe7a9993bc0fc3f21e61d
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-05-18 15:14:22 -0400

    Create smallerNumbersThanCurrent.js

---
+++ smallerNumbersThanCurrent.js
@@ -0,0 +1,10 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        for i in range(n):
+            for j in range(n):
+                if nums[i]>nums[j]:
+                    res[i] += 1
+        return res
+        
