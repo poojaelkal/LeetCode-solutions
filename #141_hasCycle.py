09c20fcc5563e9fa3deac4d8a052f1c2402791f5
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-05-19 15:49:05 -0400

    committing #141 solution

---
+++ #141_hasCycle.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        while head and head.val:
+            head.val = None
+            head = head.next
+        return head != None
