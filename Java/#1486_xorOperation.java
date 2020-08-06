d9fbbec9514713f57460d2c7c9915b54410d0189
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-08-06 10:44:41 -0400

    commenting #1486 solution

---
+++ Java/#1486_xorOperation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        int xor=0;
+        for(int i=0;i<n;i++){
+            nums[i] = start+2*i;
+        }
+        for(int i=0;i<nums.length;i++){
+            xor = xor ^ nums[i];
+        }
+        return xor;
+        
+    }
+}
