60299c0afb0080f50a41171a7ee24511ffb1ca82
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-08-07 10:42:28 -0400

    Update #1450_busyStudent.py

--- Python/#1450_busyStudent.py
+++ Python/#1450_busyStudent.py
@@ -3,9 +3,7 @@ class Solution:
         count =0;
         for i in range(len(startTime)):
             if startTime[i]==queryTime or endTime[i]==queryTime:
-                print("second")
                 count+=1
             elif startTime[i]<queryTime and endTime[i]>queryTime:
-                print("forth")
                 count+=1
         return count
