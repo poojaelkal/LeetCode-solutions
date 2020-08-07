c1f2a6b28af28f2ae7e1194eae59c702d20431d4
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2020-08-07 14:51:01 -0400

    commenting #1502 solution

---
+++ Python/#1502_canMakeArithmeticProgression.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        diff = arr[1]-arr[0]
+        for i in range(1, len(arr)-1):
+            if diff!=arr[i+1]-arr[i]:
+                return False
+        return True
