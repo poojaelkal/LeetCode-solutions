c96e27bfd1f2b2ef77a2d7481d73f591fda24803
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2021-03-20 18:35:15 -0400

    committing #204 solution

---
+++ Java/#204_countPrimes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]==false){
+                count++;
+                for(int j=i;j<n;j=j+i){
+                    prime[j]=true;
+                }
+            }
+        }
+        return count;
+    }
+}
