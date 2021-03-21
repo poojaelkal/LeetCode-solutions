738ea2e175b1cd8070b183bc8d79dbbf535106db
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2021-03-21 16:16:46 -0400

    committing #88 solution

---
+++ Java/#88_merge.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int t1=m-1;
+        int t2=n-1;
+        int fin=m+n-1;
+        while(t1>=0 && t2>=0){
+            nums1[fin--] = (nums1[t1]>nums2[t2])?nums1[t1--]:nums2[t2--];
+        }
+        while(t2>=0){
+            nums1[fin--]=nums2[t2--];
+        }
+        
+    }
+}
