cddce05573f011aeeac10b190f2c2bce0b7aad48
poojaelkal <49996647+poojaelkal@users.noreply.github.com>
2021-03-21 21:32:36 -0400

    committing #242 solution

---
+++ Java/#242_isAnagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] c=new int[26];
+        for(int i=0;i<s.length();i++){
+            c[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            c[t.charAt(i)-'a']--;
+            if(c[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(c[i]!=0) return false;
+        }
+        return true;
+    }
+}
